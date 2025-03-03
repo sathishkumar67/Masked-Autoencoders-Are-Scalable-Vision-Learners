60d33ce90c866ea2f7a60c66a88eeabd5cea9d77
sathishkumar67 <sathishvillers2000@gmail.com>
2025-03-03 15:20:45 +0530

    created model file

---
+++ model.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Tuple
+import gin
+import torch
+import torch.nn as nn
+
+@gin.configurable
+@dataclass
+class MAEConfig:
+    lr: float 
+    batch_size: int
+    epochs: int
+    device: str
+    weight_decay: float
+    eps: float
+    seed : int
+    betas: Tuple[float, float]
+    gpu_count: int
+    num_workers: int
+    pin_memory: bool
+    
+    def __post_init__(self):
+        pass
+    
+class MAE(nn.Module):
+    def __init__(self, encoder: nn.Module, decoder: nn.Module) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, x) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        encoder_op, mask, ids_restore = self.encoder(x)
+        decoder_op = self.decoder((encoder_op, mask, ids_restore), x)
+        return decoder_op
